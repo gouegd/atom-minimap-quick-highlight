c302c10a9cee2db068d59ee5b1badbc8ec00c38c
Gregory Desfour <gregory.desfour@accedo.tv>
2017-01-01 18:55:45 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+# minimap-quick-highlight package for Atom
+
+An integration between minimap and quick-highlight for the Atom text editor.
+
+Early stages - does not handle quick-highlight toggles yet, only the current selection.
---
+++ spec/minimap-quick-highlight-spec.js
@@ -0,0 +1,29 @@
+var MinimapQuickHighlight = require('../lib/minimap-quick-highlight');
+
+describe("MinimapQuickHighlight", function() {
+  var editor,
+      ref,
+      workspaceElement;
+
+  beforeEach(function() {
+    workspaceElement = atom.views.getView(atom.workspace);
+    jasmine.attachToDOM(workspaceElement);
+    waitsForPromise(function() { return atom.workspace.open('sample.js'); });
+
+    runs(function() {
+      editor = atom.workspace.getActiveTextEditor();
+      editor.setText("This is the file content");
+    });
+    waitsForPromise(function() {
+      return atom.packages.activatePackage('minimap');
+    });
+    return waitsForPromise(function() {
+      return atom.packages.activatePackage('minimap-quick-highlight');
+    });
+  });
+  describe("with an open editor that have a minimap", function() {
+    it("lives", function() {
+      expect('life').toBe('easy');
+    });
+  });
+});
